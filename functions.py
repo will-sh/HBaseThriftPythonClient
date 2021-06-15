3a54f3e7fc121b7eaab976060f25f2a0230f3cfd
root <root@will-application.novalocal>
2021-06-15 08:55:51 +0000

    First commit

---
+++ functions.py
@@ -0,0 +1,117 @@
+from thrift.transport import TSocket
+from thrift.protocol import TBinaryProtocol
+from thrift.transport import TTransport
+from hbase import Hbase
+from subprocess import call
+
+def test():
+  print("I'm test in functions.py")
+
+def kinit():
+  krb5_server='c1677-node1.coelab.cloudera.com'
+  krb_service='hbase'
+  principal='hbase/c1677-node4.coelab.cloudera.com'
+  keytab="/root/HBaseThrift/c1677.keytab"
+  kinitCommand="kinit"+" "+"-kt"+" "+keytab+" "+principal
+  call(kinitCommand,shell="True")
+
+def connect(thrift_host,thrift_port):
+  socket = TSocket.TSocket(thrift_host, thrift_port)
+  transport = TTransport.TSaslClientTransport(
+    socket,
+    host=thrift_host,
+    service='hbase',
+    mechanism='GSSAPI'
+  )
+  protocol = TBinaryProtocol.TBinaryProtocol(transport)
+  client = Hbase.Client(protocol)
+  return client,transport
+
+
+def getAllTables(client):
+  alltables = client.getTableNames()
+  tables=[]
+  for t in alltables:
+    tables.append(t.decode())
+  return tables
+
+def showAllTables(client):
+  tables=getAllTables(client)
+  print(tables,"\n")
+
+def searchTable(client,tablename):
+  tables = getAllTables(client)
+  found = False
+  for table in tables:
+    if table == tablename:
+      found = True
+    return found
+
+def createTable(client):
+  tablename=input("Enter table name:\n")
+  cfname=input("Enter cf name (e.g. cf:a):\n")
+  found=searchTable(client,tablename)
+  try:
+    client.createTable(tablename.encode(), [Hbase.ColumnDescriptor(name=cfname.encode())])
+    print(tablename + " is created!")
+  except:
+    print(tablename + " is already exists!")
+
+def istableenabled(client,tablename):
+  istblenabled=client.isTableEnabled(tablename.encode())
+  return istblenabled
+
+def enableTable(client):
+  tablename=input("Enter table name:\n")
+  if istableenabled(client,tablename):
+    print(tablename+" is already enabled\n")
+  else:
+    try:
+      client.enableTable(tablename.encode())
+      print(tablename+" is enabled!\n")
+    except:
+      print("cannot enable "+tablename+"\n")
+
+def disableTable(client):
+  tablename=input("Enter table name:\n")
+  if istableenabled(client,tablename):
+    try:
+      client.disableTable(tablename.encode())
+      print(tablename+" is disabled\n")
+    except:
+      print("cannot disable "+tablename)
+
+def deleteTable(client):
+  tablename=input("Enter table name:\n")
+  if istableenabled(client,tablename):
+    print("cannot delete "+tablename+" because it is enabled! Please disabled it firstly!\n")
+  else:
+    try:
+      client.deleteTable(tablename.encode())
+      print(tablename+" is deleted\n")
+    except:
+      print(tablename + "cannot be deleted\n")
+
+def mutateRows(client):
+  tablename=str(input('Input Table Name:\n'))
+  rowkey=str(input('Input Rowkey:\n'))
+  cfname=str(input('Input CF Name:\n'))
+  columnvalue=str(input('Input Column Value:\n'))
+  mutations = [Hbase.Mutation(column=cfname.encode(), value=columnvalue.encode())]
+  client.mutateRow(tablename.encode(), rowkey.encode(), mutations,None)
+
+def getRows(client):
+  tablename=str(input('Input Table Name:\n'))
+  rowkey=str(input('Input Rowkey:\n'))
+  cfname=str(input('Input CF Name:\n'))
+  rows=client.getRow(tablename.encode(),rowkey.encode(),None)
+  print(rows)
+  for row in rows:
+    cv=row.columns.get(cfname.encode()).value
+    print("RowKey="+row.row.decode(),"ColumnValue="+cv.decode())
+
+def deleteRows(client):
+  tablename=str(input('Input Table Name:\n'))
+  rowkey=str(input('Input Rowkey:\n'))
+  client.deleteAllRow(tablename.encode(),rowkey.encode(), attributes =None)
+  print("The row of "+rowkey+" is deleted\n")
---
+++ menu.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""
+Demonstration example for GitHub Project at
+https://github.com/IngoMeyer441/simple-term-menu
+
+This code only works in python3. Install per
+
+    sudo pip3 install simple-term-menu
+
+"""
+import time
+import functions as f
+from simple_term_menu import TerminalMenu
+
+#CDP6.3.4 kerberos enabled
+hbase_thrift_server_host = 'c1677-node4.coelab.cloudera.com'
+hbase_thrift_server_port = 9090
+
+def main(client):
+    main_menu_title = "Python App- Interact with HBase Thrift Proxy\n"
+    main_menu_items = ["[1] Show All Tables", "[2] Create Table", "[3] Enable Table", "[4] Disable Table", "[5] Delete Table","[6] Manipulate Table","[7] Quit"]
+    main_menu_cursor = "> "
+    main_menu_cursor_style = ("fg_red", "bold")
+    main_menu_style = ("standout",)
+    main_menu_exit = False
+
+    main_menu = TerminalMenu(
+        menu_entries=main_menu_items,
+        title=main_menu_title,
+        menu_cursor=main_menu_cursor,
+        menu_cursor_style=main_menu_cursor_style,
+        menu_highlight_style=main_menu_style,
+        cycle_cursor=True,
+        clear_screen=True,
+    )
+
+    manipulatetable_menu_title = "  Manipulate Table\n"
+    manipulatetable_menu_items = ["[1] Get Row", "[2] Delete Row", "[3] Mutate Rows","[4] Back to Main Menu"]
+    manipulatetable_menu_back = False
+    manipulatetable_menu = TerminalMenu(
+        manipulatetable_menu_items,
+        title=manipulatetable_menu_title,
+        menu_cursor=main_menu_cursor,
+        menu_cursor_style=main_menu_cursor_style,
+        menu_highlight_style=main_menu_style,
+        cycle_cursor=True,
+        clear_screen=True,
+    )
+
+    while not main_menu_exit:
+        main_sel = main_menu.show()
+
+        if main_sel == 0:
+            print(main_menu_items[0],"\n")
+            f.showAllTables(client)
+            input("Press Enter to go back...")
+            time.sleep(1)
+        elif main_sel == 1:
+            print(main_menu_items[1])
+            f.createTable(client)
+            input("Press Enter to go back...")
+            time.sleep(1)
+        elif main_sel == 2:
+            print(main_menu_items[2])
+            f.enableTable(client)
+            input("Press Enter to go back...")
+            time.sleep(1)
+        elif main_sel == 3:
+            print(main_menu_items[3])
+            f.disableTable(client)
+            input("Press Enter to go back...")
+            time.sleep(1)
+        elif main_sel == 4:
+            print(main_menu_items[4])
+            f.deleteTable(client)
+            input("Press Enter to go back...")
+            time.sleep(1)
+        elif main_sel == 5:
+            while not manipulatetable_menu_back:
+                manipulatetable_menu_sel = manipulatetable_menu.show()
+                if manipulatetable_menu_sel == 0:
+                    print(manipulatetable_menu_items[0])
+                    f.getRows(client)
+                    input("Press Enter to go back...")
+                    time.sleep(1)
+                elif manipulatetable_menu_sel == 1:
+                    print("[2] Delete Row")
+                    f.deleteRows(client)
+                    input("Press Enter to go back...")
+                    time.sleep(1)
+                elif manipulatetable_menu_sel == 2:
+                    print(manipulatetable_menu_items[2])
+                    f.mutateRows(client)
+                    input("Press Enter to go back...")
+                    time.sleep(1)
+                elif manipulatetable_menu_sel == 3:
+                    manipulatetable_menu_back = True
+                    print("Back to Main Menu")
+        elif main_sel == 6:
+            main_menu_exit = True
+            print("Thanks for playing! Bye!")
+            time.sleep(1)
+if __name__ == "__main__":
+    f.kinit()
+    client,transport=f.connect(hbase_thrift_server_host,hbase_thrift_server_port)
+    transport.open()
+    main(client)
+    transport.close()
